189e0dd853cbacb20d1ea3108d58e5701441999e
Hassan <hasanahmad9812021@gmail.com>
2024-03-30 19:46:39 +0600

    BMI Calculator

--- lib/network_response.dart
+++
@@ -1,7 +0,0 @@
-class NetworkResponse {
-  final int statusCode;
-  final bool isSuccess;
-  final Map<String, dynamic>? responseJson;
-
-  NetworkResponse(this.isSuccess, this.statusCode, this.responseJson);
-}
--- lib/urls.dart
+++
@@ -1,6 +0,0 @@
-class Urls {
-  static const String _facultyBaseUrl = 'https://10.0.2.2:2005/api/teacher';
-  static const String facultyReg = '$_facultyBaseUrl/Registration';
-}
-
-//https://10.0.2.2:2005/api/v1
