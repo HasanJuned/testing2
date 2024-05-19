8d2db8113871a4804f109c3e1fa544e87fd7f42e
Hassan <hasanahmad9812021@gmail.com>
2024-05-19 17:27:16 +0600

    Bus Logic updated

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,9 +1,7 @@
 class Urls {
    static const String _flutterCourseBaseUrl = 'http://10.0.2.2:2001/flutterCourseByHasan';
-  // static const String _flutterCourseBaseUrl = 'http://localhost:2001/flutterCourseByHasan';
    static const String login = '$_flutterCourseBaseUrl/login';
    static const String signUp = '$_flutterCourseBaseUrl/signUp';
-  // static String playVideo(id) => '$_flutterCourseBaseUrl/fetchVideo/$id';
 
 
   static const String _videoBaseAssets = 'assets/videos/uploads';
