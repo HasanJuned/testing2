64df91984609e2af19188c5cfed0e785fe2fe9e3
Hassan <hasanahmad9812021@gmail.com>
2024-05-24 14:32:10 +0600

    practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,62 @@
-import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'bmi_calculator.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: false,
-      builder: (context) => const MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(100, 1000),
-      builder: (context, child) => GetMaterialApp(
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        debugShowCheckedModeBanner: false,
-        navigatorKey: MyApp.globalKey,
-        home: const BmiCalculator(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text('Home Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: ElevatedButton(onPressed: (){
+
+            showDialog(context: context, builder: (context){
+
+              return AlertDialog(
+                title: Text('Warning'),
+                content: Text('Are you sure for confirmation'),
+                actions: [
+                  TextButton(onPressed: (){
+
+                  }, child: Text('No')),
+                  TextButton(onPressed: (){
+
+
+                  }, child: Text('Yes')),
+                ],
+              );
+
+
+            });
+
+
+          }, child: Text('Submit'),)
+
+
+
+        ),
       ),
     );
   }
